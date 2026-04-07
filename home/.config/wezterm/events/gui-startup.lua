7420000d7df57ef6233067d7e189d76bae5bb942
oooodnakov <coolkasodnakov@gmail.com>
2026-04-07 19:56:59 +0300

    Restore WezTerm PowerShell startup default

--- home/.config/wezterm/events/gui-startup.lua
+++ home/.config/wezterm/events/gui-startup.lua
@@ -1,5 +1,6 @@
 local wezterm = require('wezterm')
 local mux = wezterm.mux
+local launch = require('config.launch')
 
 local M = {}
 
@@ -15,6 +16,10 @@ M.setup = function()
 
       if startup_cwd and spawn.cwd == nil then
          spawn.cwd = startup_cwd
+      end
+
+      if spawn.args == nil and launch.default_prog and #launch.default_prog > 0 then
+         spawn.args = launch.default_prog
       end
 
       mux.spawn_window(spawn)
