a001fadb33395562365e54c1e1d1e654947568b5
a.odnakov <a.odnakov@tbank.ru>
2026-04-22 22:53:10 +0300

    style(sketchybar): group right controls

--- home/.config/sketchybar/brackets.lua
+++ home/.config/sketchybar/brackets.lua
@@ -1,10 +1,12 @@
 local colors = require("colors")
 
 -- Right items bracket
-sbar.add("bracket", "rightItems", { "volume", "battery", "wifi" }, {
+sbar.add("bracket", "rightItems", { "cpu", "memory", "volume", "battery", "wifi" }, {
 	background = {
 		color = colors.BACKGROUND,
-		corner_radius = 10,
+		border_color = colors.BACKGROUND_DARK,
+		border_width = 1,
+		corner_radius = 12,
 		height = 30,
 	},
 })
