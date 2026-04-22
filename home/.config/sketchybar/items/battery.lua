e9bf8ccc6fc837f2dd9812ec8cc7f8f7a6a72673
a.odnakov <a.odnakov@tbank.ru>
2026-04-22 22:52:03 +0300

    feat(sketchybar): add battery and volume controls

--- home/.config/sketchybar/items/battery.lua
+++ home/.config/sketchybar/items/battery.lua
@@ -1,14 +1,93 @@
 local colors = require("colors")
 
+local function set_popup_label(item, value)
+	item:set({
+		label = {
+			string = value,
+			font = "MesloLGSDZ Nerd Font Mono:Regular:12.0",
+			color = colors.TEXT_WHITE,
+			padding_left = 4,
+		},
+	})
+end
+
 local battery = sbar.add("item", "battery", {
 	position = "right",
 	update_freq = 120,
+	popup = {
+		background = {
+			color = colors.BACKGROUND,
+			border_width = 2,
+			border_color = colors.TEXT_GREY,
+			corner_radius = 10,
+		},
+		y_offset = 5,
+	},
 })
 
-battery:subscribe({ "routine", "system_woke", "power_source_change" }, function(env)
-	sbar.exec("pmset -g batt", function(batt_info)
+local percent_item = sbar.add("item", {
+	position = "popup." .. battery.name,
+	icon = {
+		string = "Charge",
+		font = "MesloLGSDZ Nerd Font Mono:Bold:12.0",
+		width = 60,
+		align = "left",
+		color = colors.TEXT_GREY,
+	},
+})
+
+local source_item = sbar.add("item", {
+	position = "popup." .. battery.name,
+	icon = {
+		string = "Source",
+		font = "MesloLGSDZ Nerd Font Mono:Bold:12.0",
+		width = 60,
+		align = "left",
+		color = colors.TEXT_GREY,
+	},
+})
+
+local eta_item = sbar.add("item", {
+	position = "popup." .. battery.name,
+	icon = {
+		string = "ETA",
+		font = "MesloLGSDZ Nerd Font Mono:Bold:12.0",
+		width = 60,
+		align = "left",
+		color = colors.TEXT_GREY,
+	},
+})
+
+local draw_item = sbar.add("item", {
+	position = "popup." .. battery.name,
+	icon = {
+		string = "Draw",
+		font = "MesloLGSDZ Nerd Font Mono:Bold:12.0",
+		width = 60,
+		align = "left",
+		color = colors.TEXT_GREY,
+	},
+})
+
+local function refresh_battery()
+	sbar.exec("pmset -g batt", function(batt_info, exit_code)
+		if exit_code ~= 0 then
+			battery:set({
+				label = {
+					string = "--",
+				},
+			})
+			set_popup_label(percent_item, "n/a")
+			set_popup_label(source_item, "n/a")
+			set_popup_label(eta_item, "n/a")
+			set_popup_label(draw_item, "n/a")
+			return
+		end
+
 		local percentage = batt_info:match("(%d+)%%")
 		local charging = batt_info:match("AC Power") ~= nil
+		local time_remaining = batt_info:match("; (%d+:%d+)[; ]")
+		local no_estimate = batt_info:match("no estimate") ~= nil
 
 		if not percentage then
 			return
@@ -18,13 +97,10 @@ battery:subscribe({ "routine", "system_woke", "power_source_change" }, function(
 		local icon = ""
 		local colour = colors.TEXT_GREY
 
-		-- Determine icon and color based on percentage
 		if percent_num >= 90 then
 			icon = ""
-			colour = colors.TEXT_GREY
 		elseif percent_num >= 60 then
 			icon = ""
-			colour = colors.TEXT_GREY
 		elseif percent_num >= 30 then
 			icon = ""
 			colour = colors.TEXT_ORANGE
@@ -36,22 +112,61 @@ battery:subscribe({ "routine", "system_woke", "power_source_change" }, function(
 			colour = colors.TEXT_RED
 		end
 
-		-- Override with charging icon if charging
 		if charging then
 			icon = ""
 		end
 
 		battery:set({
-			icon = { 
-				string = icon, 
-				color = colour, 
-    			font = "MesloLGSDZ Nerd Font Mono:Regular:13.0"
-			},
-			label = { 
-				string = percentage .. "%", 
+			icon = {
+				string = icon,
 				color = colour,
-    			font = "MesloLGSDZ Nerd Font Mono:Regular:13.0",
+				font = "MesloLGSDZ Nerd Font Mono:Regular:13.0",
+			},
+			label = {
+				string = percentage .. "%",
+				color = colour,
+				font = "MesloLGSDZ Nerd Font Mono:Regular:13.0",
 			},
 		})
+
+		set_popup_label(percent_item, percentage .. "%")
+		set_popup_label(source_item, charging and "AC power" or "Battery")
+
+		if no_estimate then
+			set_popup_label(eta_item, "n/a")
+		else
+			set_popup_label(eta_item, time_remaining or "n/a")
+		end
+
+		sbar.exec(
+			[[ioreg -r -c AppleSmartBattery -l | awk -F'= ' '/"InstantAmperage" =/ { amp=$2 } /"Voltage" =/ && !volt { volt=$2 } END { gsub(/ /, "", amp); gsub(/ /, "", volt); if (amp == "" || volt == "") exit 1; if (amp > 9223372036854775807) amp = amp - 18446744073709551616; watts = (amp * volt) / 1000000; if (watts < 0) watts = -watts; printf "%.1fW\n", watts; }']],
+			function(draw_info, draw_exit_code)
+				if draw_exit_code ~= 0 then
+					set_popup_label(draw_item, "n/a")
+					return
+				end
+
+				local draw = draw_info:match("([%d%.]+W)")
+				set_popup_label(draw_item, draw or "n/a")
+			end
+		)
 	end)
+end
+
+battery:subscribe({ "routine", "system_woke", "power_source_change" }, function()
+	refresh_battery()
 end)
+
+battery:subscribe("mouse.entered", function()
+	battery:set({ popup = { drawing = true } })
+end)
+
+battery:subscribe("mouse.exited.global", function()
+	battery:set({ popup = { drawing = false } })
+end)
+
+battery:subscribe("mouse.exited", function()
+	battery:set({ popup = { drawing = false } })
+end)
+
+refresh_battery()
--- home/.config/sketchybar/items/volume.lua
+++ home/.config/sketchybar/items/volume.lua
@@ -2,10 +2,48 @@ local colors = require("colors")
 
 local volume = sbar.add("item", "volume", {
 	position = "right",
+	popup = {
+		background = {
+			color = colors.BACKGROUND,
+			border_width = 2,
+			border_color = colors.TEXT_GREY,
+			corner_radius = 10,
+		},
+		y_offset = 5,
+	},
 })
 
-volume:subscribe("volume_change", function(env)
-	local volume_info = tonumber(env.INFO) or 0
+local volume_popup_hovered = false
+local volume_hide_token = 0
+
+local volume_slider = sbar.add("slider", "volume.slider", 90, {
+	position = "popup." .. volume.name,
+	icon = {
+		drawing = "off",
+	},
+	label = {
+		drawing = "off",
+	},
+	slider = {
+		percentage = 0,
+		highlight_color = colors.TEXT_WHITE,
+		background = {
+			height = 6,
+			corner_radius = 3,
+			color = colors.BACKGROUND_DARKER,
+		},
+		knob = {
+			string = "󰀁",
+			font = "MesloLGSDZ Nerd Font Mono:Regular:12.0",
+			color = colors.TEXT_WHITE,
+		},
+	},
+	background = {
+		drawing = "off",
+	},
+})
+
+local function refresh_volume(volume_info)
 	local icon = "󰖀"
 	if volume_info == 0 then
 		icon = "󰖁"
@@ -14,16 +52,70 @@ volume:subscribe("volume_change", function(env)
 	end
 
 	volume:set({
-		icon = { 
+		icon = {
 			string = icon,
-			font = "MesloLGSDZ Nerd Font Mono:Regular:13.0"
+			font = "MesloLGSDZ Nerd Font Mono:Regular:13.0",
 		},
-		label = { 
-			string = string.format("%d%%", volume_info) ,
-			font = "MesloLGSDZ Nerd Font Mono:Regular:13.0"
+		label = {
+			string = string.format("%d%%", volume_info),
+			font = "MesloLGSDZ Nerd Font Mono:Regular:13.0",
 		},
 	})
+
+	volume_slider:set({
+		slider = {
+			percentage = volume_info,
+		},
+	})
+end
+
+local function show_volume_popup()
+	volume_popup_hovered = true
+	volume_hide_token = volume_hide_token + 1
+	volume:set({ popup = { drawing = true } })
+end
+
+local function schedule_volume_popup_hide()
+	volume_popup_hovered = false
+	volume_hide_token = volume_hide_token + 1
+	local hide_token = volume_hide_token
+
+	sbar.exec("sleep 1", function()
+		if volume_popup_hovered or hide_token ~= volume_hide_token then
+			return
+		end
+
+		volume:set({ popup = { drawing = false } })
+	end)
+end
+
+volume:subscribe("volume_change", function(env)
+	local volume_info = tonumber(env.INFO) or 0
+	refresh_volume(volume_info)
 end)
 
--- Trigger initial update
+volume_slider:subscribe("mouse.clicked", function(env)
+	local percentage = math.max(0, math.min(100, math.floor((tonumber(env.PERCENTAGE) or 0) + 0.5)))
+
+	sbar.exec("osascript -e 'set volume output volume " .. percentage .. "'", function()
+		refresh_volume(percentage)
+	end)
+end)
+
+volume:subscribe("mouse.entered", function()
+	show_volume_popup()
+end)
+
+volume_slider:subscribe("mouse.entered", function()
+	show_volume_popup()
+end)
+
+volume:subscribe("mouse.exited.global", function()
+	schedule_volume_popup_hide()
+end)
+
+volume_slider:subscribe("mouse.exited.global", function()
+	schedule_volume_popup_hide()
+end)
+
 sbar.trigger("volume_change")
