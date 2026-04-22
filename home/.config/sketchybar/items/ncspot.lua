7938b3b5f880ea01ceeea1f0f9913c774c808ced
a.odnakov <a.odnakov@tbank.ru>
2026-04-22 23:12:35 +0300

    style(sketchybar): align ncspot popup

--- home/.config/sketchybar/items/ncspot.lua
+++ home/.config/sketchybar/items/ncspot.lua
@@ -1,71 +1,74 @@
 local colors = require("colors")
 
 local ncspot = sbar.add("item", "ncspot", {
-	position = "e",
+	position = "right",
 	drawing = "off",
 	icon = {
 		string = "",
-		font = "MesloLGSDZ Nerd Font Mono:Italic:13.0",
-		padding_left = 8,
+		font = "MesloLGSDZ Nerd Font Mono:Regular:13.0",
+		padding_left = 4,
+		padding_right = 2,
 		color = colors.TEXT_SPOTIFY_GREEN,
-	},
-	background = {
-		corner_radius = 10,
-		color = colors.BACKGROUND_DARK,
-		height = 30,
 	},
 	label = {
 		font = "MesloLGSDZ Nerd Font Mono:Regular:13.0",
 		color = colors.TEXT_WHITE,
-		max_chars = 8,
-		padding_right = 8,
+		max_chars = 16,
+		padding_left = 2,
+		padding_right = 4,
 	},
 	scroll_texts = true,
 	click_script = "ncspot-controller playpause",
 	popup = {
 		background = {
-			border_width = 0,
-			border_color = 0x000000,
+			color = colors.BACKGROUND,
+			border_width = 2,
+			border_color = colors.TEXT_GREY,
 			corner_radius = 10,
 		},
 		align = "center",
-		y_offset = 0,
+		y_offset = 5,
 	},
 })
 
--- Add popup image item
 local img = sbar.add("item", {
 	position = "popup." .. ncspot.name,
+	y_offset = 0,
+	height = 180,
 	background = {
 		drawing = "on",
 		image = {
 			drawing = "on",
 			corner_radius = 10,
-			scale = 0.5,
+			scale = 0.55,
 		},
 		corner_radius = 10,
 	},
 })
 
-sbar.add("item", "ncspot.img", {
-	position = "popup." .. ncspot.name,
-	background = {
-		drawing = "on",
-		image = {
-			drawing = "on",
-			corner_radius = 10,
-			scale = 0.5,
-		},
-		corner_radius = 10,
-	},
-})
+local function show_ncspot_popup()
+	sbar.animate("tanh", 12, function()
+		ncspot:set({ popup = { drawing = true } })
+	end)
+end
 
-ncspot:subscribe("mouse.entered", function(env)
-	ncspot:set({ popup = { drawing = "toggle" } })
+local function hide_ncspot_popup()
+	sbar.animate("tanh", 12, function()
+		ncspot:set({ popup = { drawing = false } })
+	end)
+end
+
+ncspot:subscribe("mouse.entered", function()
+	show_ncspot_popup()
 	img:set({ background = { image = { string = "/tmp/ncspot-controller-cover.jpg" } } })
 end)
 
-ncspot:subscribe("mouse.exited", function(env)
-	ncspot:set({ popup = { drawing = false } })
+ncspot:subscribe("mouse.exited.global", function()
+	hide_ncspot_popup()
+	img:set({ background = { image = { string = "" } } })
+end)
+
+ncspot:subscribe("mouse.exited", function()
+	hide_ncspot_popup()
 	img:set({ background = { image = { string = "" } } })
 end)
