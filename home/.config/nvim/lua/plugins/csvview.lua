b965b0733cb81024cc94aebac30895cfced887cc
a.odnakov <a.odnakov@tbank.ru>
2026-05-21 09:12:51 +0300

    cssview nvim

--- home/.config/nvim/lua/plugins/csvview.lua
+++ home/.config/nvim/lua/plugins/csvview.lua
@@ -1,6 +1,34 @@
 -- csvview.nvim: CSV/TSV file viewer
 return {
-  "hat0uma/csvview.nvim",
-  ft = { "csv", "tsv", "CSV", "TSV" },
-  opts = {},
+	"hat0uma/csvview.nvim",
+	---@module "csvview"
+	---@type CsvView.Options
+	ft = { "csv", "tsv" }, -- 1. Load the plugin automatically for these filetypes
+	opts = {
+		display_mode = "border",
+		parser = { comments = { "#", "//" } },
+		keymaps = {
+			-- Text objects for selecting fields
+			textobject_field_inner = { "if", mode = { "o", "x" } },
+			textobject_field_outer = { "af", mode = { "o", "x" } },
+			-- Excel-like navigation:
+			jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+			jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+			jump_next_row = { "<Enter>", mode = { "n", "v" } },
+			jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+		},
+	},
+	config = function(_, opts)
+		-- 2. Run the normal setup with your choices
+		require("csvview").setup(opts)
+
+		-- 3. Automatically turn on the columnar view when a CSV/TSV file is loaded
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "csv", "tsv" },
+			desc = "Auto-enable csvview",
+			callback = function()
+				require("csvview").enable()
+			end,
+		})
+	end,
 }
